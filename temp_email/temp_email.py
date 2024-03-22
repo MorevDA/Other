09f2368cba63ebca3bd3f775170c74064a4ae63f
Dmitry <morevda1983@gmail.com>
2024-03-22 22:46:53 +0300

    Добавлено описание функций.

--- temp_email/temp_email.py
+++ temp_email/temp_email.py
@@ -9,34 +9,46 @@ __all__ = (
 
 
 class FakeEmail:
-    base_url = "https://api.mailslurp.com"
-    session = requests.session()
+    """ Класс для взаимодействия с API mailslurp.com"""
 
+    base_url = "https://api.mailslurp.com"
+    """Базовый адрес для взаимодействия с API"""
     def __init__(self, apy_key, address=None):
-        self.headers = {'x-api-key': apy_key, 'content-type': 'application/json'}
+        """Создание нового случайного адреса электронной почты.
+           :param apy_key: Ключ полученный при регистрации на сервисе mailslurp.com
+           :param address: Опциональный параметр, указывается при наличии созданного ранее почтового адреса.
+        """
+
+        self.session = requests.session()
+        self.headers = {'x-api-key': apy_key}
         self.address = address or self.create_email()
         self.inbox_id = self.address.split('@')[0]
 
     def create_email(self) -> str:
+        """Функция для создания нового почтового ящика"""
         response = self.session.post(f'{self.base_url}/inboxes', headers=self.headers)
         response.raise_for_status()
         return response.json().get('emailAddress')
 
     def delete_inbox(self) -> int:
+        """Функция для удаления почтового ящика"""
         response = self.session.delete(f'{self.base_url}/inboxes/{self.inbox_id}', headers=self.headers)
         response.raise_for_status()
         return response.status_code
 
     def get_all_emails(self) -> List["'FakeEmail.MessageInfo'"]:
+        """Функция для получения списка всех ранее созданных пользователем почтовых ящиков"""
         response = self.session.get(f'{self.base_url}/emails', headers=self.headers)
         response.raise_for_status()
         return [FakeEmail.MessageInfo.from_dict(self, msg) for msg in response.json()['content']]
 
     def get_email_by_id(self, email_id: str) -> 'FakeEmail.MessageInfo':
+        """Функция для получения информации об электронном письме по его id"""
         response = self.session.get(f'{self.base_url}/emails/{email_id}', headers=self.headers)
         return FakeEmail.MessageInfo.from_dict(self, response.json())
 
     def get_links_in_email(self, email_id: str) -> list[str]:
+        """Функция для получения всех ссылок содержащихся в тексте электронного письма по его id"""
         response = self.session.get(f'{self.base_url}/emails/{email_id}/links', headers=self.headers)
         try:
             return response.json()['links']
@@ -45,15 +57,18 @@ class FakeEmail:
             return self.get_link_in_text(text)
 
     def get_url_for_browser(self, email_id: str) -> str:
+        """Функция для получения ссылки для просмотра электронного письма в браузере по его id"""
         response = self.session.get(f'{self.base_url}/emails/{email_id}/urls', headers=self.headers)
         return response.json()['plainHtmlBodyUrl']
 
     def get_email_text(self, email_id: str):
+        """Функция для получения текста электронного письма по его id"""
         response = self.session.get(f'{self.base_url}/emails/{email_id}/textLines', headers=self.headers)
         return ",".join(response.json()['lines'])
 
     def download_attachment(self, email_id, attachment_id: str,
                             path: str = None, file_name: str = None) -> None:
+        """Функция для сохранения приложения по его id к электронному письму"""
         response = self.session.get(f'{self.base_url}/emails/{email_id}/attachments/{attachment_id}',
                                     headers=self.headers)
         if path and file_name:
@@ -68,17 +83,20 @@ class FakeEmail:
             self.write_file(default_file_name, response.content)
 
     def get_attachment_metadata(self, email_id: str, attachment_id: str):
+        """Функция для получения информации к электронному письму по его id"""
         response = self.session.get(f'{self.base_url}/emails/{email_id}/attachments/{attachment_id}/metadata',
                                     headers=self.headers)
-        return FakeEmail.Content.from_dict(response.json())
+        return FakeEmail.Attachment.from_dict(response.json())
 
     @staticmethod
     def get_link_in_text(text: str) -> list:
+        """Функция для получения списка ссылок содержащегося в тексте"""
         link = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',  text)
         return link
 
     @staticmethod
     def write_file(file: str, content: bytes):
+        """Функция для сохранения файла"""
         with open(file, "wb") as f:
             f.write(content)
 
@@ -110,7 +128,7 @@ class FakeEmail:
         pass
 
     @dataclass
-    class Content:
+    class Attachment:
         name: str
         content_type: str
 
