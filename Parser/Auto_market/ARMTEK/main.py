f6ce23e130ffe51070e5bd58cd1f7373ba894268
Dmitry <morevda1983@gmail.com>
2024-05-07 23:31:28 +0300

    В связи с изменением структуры проекта актуализированы импорты и функционал

--- Parser/Auto_market/ARMTEK/main.py
+++ Parser/Auto_market/ARMTEK/main.py
@@ -1,40 +1,19 @@
 import requests
 
+from pprint import pprint
+
 
 from armtek_config import Armtek_Config
-from parse_original import get_parts_list
+from shemas import Shop, Part, Suggestion
+from original_parts import get_related_parts_info, get_search_info
 
 session = requests.Session()
 
 
-def get_search_info(sess, config) -> dict:
-    """Функция для получения характеристик поиска от API Ar"""
-    content = sess.post(config.preliminary_search_url,
-                        headers=config.headers_search,
-                        json=config.preliminary_search_data).json()
-    search_info = {}
-    parts_info = content["data"]  # ["data"]['articlesData'])
-    search_info['cacheKey'] = parts_info['cacheKey']
-    suggestion = parts_info['articlesData'][0]['SUGGESTIONS']
-    search_info['keyzaks'] = [i['KEYZAK'] for i in suggestion]
-    search_info['artId'] = list(set(i['ARTID'] for i in suggestion))[0]
-    return search_info
+Arm: Shop = Shop("armtek")
 
+get_search_info(session, Armtek_Config, Arm, Part, Suggestion)
+get_related_parts_info(session, Armtek_Config, Arm, Suggestion)
 
-def get_original_parts_info(sess, config) -> list:
-    """Функция для получения списка предложений оригинальных деталей"""
-    content_data = sess.post(config.final_search_url,
-                             headers=config.headers_search,
-                             json=config.final_search_data).json()
-    try:
-        return get_parts_list(content_data)
-    except:
-        print('Ooops')
-
-
-if __name__ == '__main__':
-    search_params = get_search_info(session, Armtek_Config)
-    Armtek_Config.final_search_data['artId'] = search_params['artId']
-    Armtek_Config.final_search_data['keyzaks'] = search_params['keyzaks']
-    print(get_original_parts_info(session, Armtek_Config))
+pprint(Arm)
 
