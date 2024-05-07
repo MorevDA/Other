0c90d21699740e71c364e7453cb1d2a6efe76169
Dmitry <morevda1983@gmail.com>
2024-05-07 22:34:11 +0300

    Скорректированы функции get_search_info и get_related_parts_info - получение результата запроса от API выделено в отдельную функцию get_content и перенесено в модуль service.py

--- Parser/Auto_market/ARMTEK/original_parts.py
+++ Parser/Auto_market/ARMTEK/original_parts.py
@@ -1,28 +1,26 @@
-from service import get_suggestion_list
+from service import get_suggestion_list, get_content
 
 
 def get_search_info(sess, config, shop, part, suggestion) -> None:
     """Функция для получения характеристик поиска и характеристик оригинальной детали
      от API Armtek"""
-    content = sess.post(config.preliminary_search_url,
-                        headers=config.headers_search,
-                        json=config.preliminary_search_data).json()
-    parts_info = content["data"]
-    parts_data = parts_info['articlesData'][0]
+    content = get_content(sess, config.preliminary_search_url,config.headers_search,
+                          config.preliminary_search_data)
+    parts_data = content["data"]['articlesData'][0]
     suggestions = parts_data['SUGGESTIONS']
     art_id = list(set(i['ARTID'] for i in suggestions))[0]
-    config.final_search_data['keyzaks'] = [i['KEYZAK'] for i in suggestions]
     config.final_search_data['artId'] = art_id
-    shop.original_parts = part(parts_data['BRAND'], parts_data['PIN'], parts_data['NAME'], art_id)
+    keyzaks = [i['KEYZAK'] for i in suggestions]
+    config.final_search_data['keyzaks'] = keyzaks
+    shop.original_parts = part(parts_data['BRAND'], parts_data['PIN'], parts_data['NAME'], art_id, keyzaks=keyzaks)
     parts_list = get_suggestion_list(suggestion, suggestions)
     shop.original_parts.suggestions = parts_list
 
 
 def get_related_parts_info(sess, config, shop, suggestion) -> None:
     """Функция для получения списка предложений оригинальных деталей"""
-    content_data = sess.post(config.final_search_url,
-                             headers=config.headers_search,
-                             json=config.final_search_data).json()['data']
+    content_data = get_content(sess, config.final_search_url, config.headers_search,
+                               config.final_search_data)['data']
     parts_list = get_suggestion_list(suggestion, content_data)
     shop.original_parts.suggestions.extend(parts_list)
 
