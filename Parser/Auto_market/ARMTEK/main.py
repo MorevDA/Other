01c0cfc0b363372a88ca2e398327a09a83f4b494
Dmitry <morevda1983@gmail.com>
2024-05-16 22:39:11 +0300

    В связи с изменением структуры проекта модуль main полностью изменен

--- Parser/Auto_market/ARMTEK/main.py
+++ Parser/Auto_market/ARMTEK/main.py
@@ -2,18 +2,19 @@ import requests
 
 from pprint import pprint
 
-
 from armtek_config import Armtek_Config
 from shemas import Shop, Part, Suggestion
-from original_parts import get_related_parts_info, get_search_info
+from original_parts import get_original_parts
+from analog_parts import get_analog_parts
 
 session = requests.Session()
 
-
 Arm: Shop = Shop("armtek")
+Armtek_Config.preliminary_search_data['query'] = Armtek_Config.preliminary_search_data['filters']['text'] = \
+    Armtek_Config.related_parts_search_data['query'] = Armtek_Config.related_parts_search_data['filters'][
+    'text'] = input(
+    "Введите парт-номер детали для поиска:_")
 
-get_search_info(session, Armtek_Config, Arm, Part, Suggestion)
-get_related_parts_info(session, Armtek_Config, Arm, Suggestion)
-
+get_original_parts(session, Armtek_Config, Arm, Part, Suggestion)
+get_analog_parts(session, Armtek_Config, Arm, Part, Suggestion)
 pprint(Arm)
-
