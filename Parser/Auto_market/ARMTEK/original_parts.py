37a1d131748301e5dc6e20e5fe595e683ee499c7
Dmitry <morevda1983@gmail.com>
2024-05-16 21:21:42 +0300

    Функции изменены в связи с изменениями в API Armtek и изменением внутренней структуры проекта

--- Parser/Auto_market/ARMTEK/original_parts.py
+++ Parser/Auto_market/ARMTEK/original_parts.py
@@ -3,25 +3,30 @@ from copy import deepcopy
 
 
 def get_search_info(sess, config, shop, part, suggestion) -> None:
-    """Функция для получения от API Armtek основных параметров для поиска"""
+    """Функция для получения от API Armtek с основными предложениями по парт-номеру
+    детали, а также внутреннего актикула Armtek для дальнейшего поиска деталей с таким
+    же парт-номером и аналогов"""
     content = get_content(sess, config.preliminary_search_url, config.headers_search,
                           config.preliminary_search_data)
     parts_data = content["data"]['articlesData'][0]
-    suggestions = parts_data['SUGGESTIONS']
+    # suggestions = parts_data['SUGGESTIONS']
     art_id = parts_data['ARTID']
-    keyzaks = [i['KEYZAK'] for i in suggestions]
-    shop.original_parts = part(parts_data['BRAND'], parts_data['PIN'], parts_data['NAME'], art_id, keyzaks=keyzaks)
+    shop.original_parts = part(parts_data['BRAND'], parts_data['PIN'], parts_data['NAME'], art_id)
 
 
 def get_related_parts_info(sess, config, shop, suggestion) -> None:
-    """Функция для получения списка предложений оригинальных деталей"""
+    """Функция для получения полного списка предложений по искомому парт-номеру."""
     final_data = deepcopy(config.final_search_data)
     final_data['artId'] = shop.original_parts.artid
-    final_data['keyzaks'] = shop.original_parts.keyzaks
     content_data = get_content(sess, config.final_search_url, config.headers_search,
                                config.final_search_data)['data']
     parts_list = get_suggestion_list(suggestion, content_data)
     shop.original_parts.suggestions.extend(parts_list)
+
+
+def get_original_parts(sess, config, shop, part, suggestion ):
+    get_search_info(sess, config, shop, part, suggestion)
+    get_related_parts_info(sess, config, shop, suggestion)
 
 
 if __name__ == '__main__':
@@ -34,7 +39,6 @@ if __name__ == '__main__':
     session = Session()
     Arm: Shop = Shop("armtek")
 
-    get_search_info(session, Armtek_Config, Arm, Part, Suggestion)
-    get_related_parts_info(session, Armtek_Config, Arm, Suggestion)
+    get_original_parts(session, Armtek_Config, Arm, Part, Suggestion)
 
     pprint(Arm)
